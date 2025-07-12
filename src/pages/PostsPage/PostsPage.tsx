dc147a814a79415f1fc5d317e061d649ef2b293d
Vladimir Volkov <53047099+ApeIsHere@users.noreply.github.com>
2025-07-12 13:39:17 +0300

    feat(usePosts): create usePosts.tsx hook, lift data-fetch and other post logic from App to PostsPage, add contants.ts

--- src/pages/PostsPage/PostsPage.tsx
+++ src/pages/PostsPage/PostsPage.tsx
@@ -1,5 +1,14 @@
+import usePosts from "../../features/PostList/model/hooks/usePosts";
+import withLoading from "../../shared/lib/hoc/withLoading";
+import PostListSkeleton from "../../shared/ui/Skeletons/PostListSkeleton";
+import PostList from "../../widgets/PostList/PostList";
+
+// Оборачиваем PostList в HOC и добавляем скелетона на время загрузки
+const PostListWithLoading = withLoading(PostList, PostListSkeleton);
+
 function PostsPage() {
-  return <div>Posts Here</div>;
+  const { posts, comments, isLoading } = usePosts();
+  return <PostListWithLoading posts={posts} isLoading={isLoading} comments={comments} />;
 }
 
 export default PostsPage;
