21263ed92a50ed572edc9575af105667ee070b93
Vladimir Volkov <53047099+ApeIsHere@users.noreply.github.com>
2025-07-15 13:30:30 +0300

    feat(PostDetailPage): add comments section

--- src/pages/PostDetailPage/PostDetailPage.tsx
+++ src/pages/PostDetailPage/PostDetailPage.tsx
@@ -2,10 +2,11 @@ import { useParams } from "react-router-dom";
 import usePosts from "../../features/PostList/model/hooks/usePosts";
 import styles from "./PostDetailPage.module.css";
 import PostCard from "../../entities/post/ui/PostCard";
+import CommentCard from "../../entities/comment/ui/CommentCard";
 
 function PostDetailPage() {
-  const { id } = useParams();
-  const { posts, isLoading } = usePosts();
+  const { id } = useParams(); // post id
+  const { posts, comments, isLoading } = usePosts();
 
   const post = posts.find((p) => p.postId === Number(id));
 
@@ -17,29 +18,37 @@ function PostDetailPage() {
     return <div className={styles.error}>Post not found</div>;
   }
 
+  const postComments = comments.filter((c) => c.postId === post?.postId);
+
   return (
     <>
       <PostCard post={post} />
       <div className={styles.container}>
         <div className={styles.body}>
           <img src="https://picsum.photos/200/360" alt="random" />
-          <p>
+          <div>
             <h3>{post.title}</h3>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur similique
-            itaque dolorem debitis suscipit atque. Nesciunt, deserunt? Quod recusandae
-            itaque maiores quisquam nisi quia laudantium laborum, qui, soluta sit earum?
-            <br />
-            <br />
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim perspiciatis,
-            voluptas voluptatem reprehenderit fugit a in quas facere, omnis dicta animi
-            libero repudiandae, sequi magnam consequatur perferendis quisquam doloremque
-            sunt. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam repellat
-            repellendus voluptate fuga nobis similique rem eius, quam quibusdam
-            reprehenderit natus illum libero officia aut corrupti quaerat. Suscipit,
-            voluptatibus at.
-          </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+              similique itaque dolorem debitis suscipit atque. Nesciunt, deserunt? Quod
+              recusandae itaque maiores quisquam nisi quia laudantium laborum, qui, soluta
+              sit earum?
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim perspiciatis,
+              voluptas voluptatem reprehenderit fugit a in quas facere, omnis dicta animi
+              libero repudiandae, sequi magnam consequatur perferendis quisquam doloremque
+              sunt. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam
+              repellat repellendus voluptate fuga nobis similique rem eius, quam quibusdam
+              reprehenderit natus illum libero officia aut corrupti quaerat. Suscipit,
+              voluptatibus at.
+            </p>
+          </div>
         </div>
       </div>
+      {postComments.map((comment) => (
+        <CommentCard key={comment.id} comment={comment} />
+      ))}
     </>
   );
 }
