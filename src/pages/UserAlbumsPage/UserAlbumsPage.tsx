9542dc2ec655a5e3250983656ca3b35984a7f276
Vladimir Volkov <53047099+ApeIsHere@users.noreply.github.com>
2025-07-16 12:36:38 +0300

    feat(UserAlbumsPage): implement userAlbumsPage.tsx, render albums list with links to photos

--- src/pages/UserAlbumsPage/UserAlbumsPage.tsx
+++ src/pages/UserAlbumsPage/UserAlbumsPage.tsx
@@ -1,5 +1,29 @@
+import useUserData from "../../features/User/model/hooks/useUserData";
+import styles from "./UserAlbumsPage.module.css";
+import { Link } from "react-router-dom";
+
 function UserAlbumsPage() {
-  return <div>User's Albums Here</div>;
+  const { userAlbums, userName } = useUserData();
+
+  // guard-clause
+  if (!userAlbums.length) {
+    return <div className={styles.empty}>No albums found for {userName}</div>;
+  }
+
+  return (
+    <div>
+      <h2 className={styles.title}>{userName}'s Albums</h2>
+      <ul className={styles.list}>
+        {userAlbums.map((album) => (
+          <li key={album.id} className={styles.album}>
+            <Link to={`/albums/${album.id}/photos`} className={styles.link}>
+              {album.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default UserAlbumsPage;
--- src/pages/UserTodosPage/UserTodosPage.tsx
+++ src/pages/UserTodosPage/UserTodosPage.tsx
@@ -6,12 +6,12 @@ function UserTodosPage() {
 
   // guard-clause
   if (!userTodos.length) {
-    return <div className={styles.empty}>No todos found</div>;
+    return <div className={styles.empty}>No todos found for {userName}</div>;
   }
 
   return (
     <div>
-      <h2>{userName} todos</h2>
+      <h2 className={styles.title}>{userName} todos</h2>
       <ul className={styles.list}>
         {userTodos.map((todo) => (
           <li
